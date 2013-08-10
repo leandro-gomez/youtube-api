980d4dde8b9b122655a7ac4a26a31c4697832549
lalo <leandro.gz73@gmail.com>
2013-08-10 01:09:32 -0300

    Added Snippet class

--- youtube/data_API/models/supporting.py
+++ youtube/data_API/models/supporting.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
 
 
+class Snippet(object):
+    def __init__(self, publishedAt=None, channelId=None, title=None, description=None,
+                 thumbnails=None, channelTitle=None, type=None, groupId=None):
+        self.publishedAt = publishedAt
+        self.channelId = channelId
+        self.title = title
+        self.description = description
+        self.thumbnails = thumbnails
+        self.channelTitle = channelTitle
+        self.type = type
+        self.groupId = groupId
+
+
 __author__ = 'lalo'
