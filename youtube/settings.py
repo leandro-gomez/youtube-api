7550242808271abc867c029b641f27bc33cc7985
lalo <leandro.gz73@gmail.com>
2013-08-10 01:09:44 -0300

    Removed unused variables

--- youtube/settings.py
+++ youtube/settings.py
@@ -8,22 +8,4 @@ RESOURCE_TYPES = (
     'videoCategory',
 )
 
-SUPPORTED_OPERATIONS = (
-    'list', 'insert', 'update', 'delete',
-)
-
-RESOURCE_TYPES_SUPPORT = {
-    'activity': {'list': True, 'insert': True, 'update': False, 'delete': False},
-    'channel': {'list': True, 'insert': False, 'update': False, 'delete': False},
-    'guideCategory': {'list': True, 'insert': False, 'update': False, 'delete': False},
-    'playlist': {'list': True, 'insert': True, 'update': True, 'delete': True},
-    'playlistItem': {'list': True, 'insert': True, 'update': True, 'delete': True},
-    'search result': {'list': True, 'insert': False, 'update': False, 'delete': False},
-    'subscription': {'list': True, 'insert': False, 'update': False, 'delete': False},
-    'video': {'list': True, 'insert': True, 'update': True, 'delete': True},
-    'videoCategory': {'list': True, 'insert': False, 'update': False, 'delete': False},
-}
-
-
-
 __author__ = 'lalo'
