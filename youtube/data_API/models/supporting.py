59381991e42a164ee120a227869f2c9b721af5e6
lalo <leandro.gz73@gmail.com>
2013-08-10 01:41:50 -0300

    Added thumbnails parse

--- youtube/data_API/models/supporting.py
+++ youtube/data_API/models/supporting.py
@@ -9,17 +9,26 @@ class Snippet(object):
         self.channelId = channelId
         self.title = title
         self.description = description
-        self.thumbnails = thumbnails
+        self._thumbnails = thumbnails
         self.channelTitle = channelTitle
         self.type = type
         self.groupId = groupId
         self.parse()
 
     def parse(self):
+        self.parse_publishedAt()
+        self.parse_thumbnails()
+
+    def parse_publishedAt(self):
         publishedAt = self._publishedAt
         if publishedAt:
             publishedAt = iso8601.parse_date(publishedAt)
             self.publishedAt = publishedAt
+
+    def parse_thumbnails(self):
+        thumbnails = self._thumbnails
+        if thumbnails:
+            self.thumbnails = Thumbnail.parse(thumbnails)
 
 
 class ContentDetails(object):
@@ -35,6 +44,20 @@ class ContentDetails(object):
         self.bulletin = bulletin
         self.social = social
         self.channelItem = channelItem
+
+
+class Thumbnail(object):
+    def __init__(self, url=None, width=None, height=None):
+        self.url = url
+        self.width = width
+        self.height = height
+
+    @classmethod
+    def parse(cls, elements):
+        parsed = dict()
+        for k, v in elements.iteritems():
+            parsed[k] = cls(**v)
+        return parsed
 
 
 __author__ = 'lalo'
