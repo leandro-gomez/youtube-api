2be9704e65b07caf0d5cbd20912d36adad883643
lgomez <leandro.gz73@gmail.com>
2013-08-16 23:50:47 -0300

    Change to version 0.2dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='youtube-api-wrapper',
-    version='0.1',
+    version='0.2dev',
     packages=['youtube', 'youtube.data', 'youtube.data.resources'],
     include_package_data=True,
     license='BSD License',
