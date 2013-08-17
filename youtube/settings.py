17a18172471662f780cbf89411404a0ca0d466f8
lgomez <leandro.gz73@gmail.com>
2013-08-17 00:06:19 -0300

    Removed unused variable

--- youtube/settings.py
+++ youtube/settings.py
@@ -3,10 +3,4 @@ import os
 
 API_KEY = os.environ.get('YOUTUBE_API_KEY', None)
 
-RESOURCE_TYPES = (
-    'activity', 'channel', 'guideCategory', 'playlist',
-    'playlistItem', 'search result', 'subscription', 'video',
-    'videoCategory',
-)
-
 __author__ = 'lalo'
